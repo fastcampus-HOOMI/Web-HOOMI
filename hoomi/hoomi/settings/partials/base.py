8e0e3ef9b378c41302870432a88dca534dfae5e0
Luke <novelview9@gmail.com>
2016-07-05 16:31:29 +0900

    install postresql : add requirements updates hoomi settings

--- hoomi/hoomi/settings/partials/base.py
+++ hoomi/hoomi/settings/partials/base.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+import dj_database_url
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 PROJECT_ROOT_DIR = os.path.dirname(BASE_DIR)
@@ -77,12 +80,17 @@ WSGI_APPLICATION = 'hoomi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABSES = {}
+DATABASES['default'] = dj_database_url.config(default=os.environ.get("DATABASE_URL"),
+)
+
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 
 # Password validation
