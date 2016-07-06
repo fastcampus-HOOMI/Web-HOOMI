755e47c1a2d27900a5b189309acffced0554f286
Luke <novelview9@gmail.com>
2016-07-07 05:46:23 +0900

    updates url : add users-url on hoomi-url

--- hoomi/hoomi/urls/__init__.py
+++ hoomi/hoomi/urls/__init__.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 from django.http.response import HttpResponse
@@ -25,4 +25,6 @@ def home(request):
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home),
+
+    url(r'^', include("users.urls", namespace='usrers')),
 ]
