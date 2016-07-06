862158610f795892cd4c6a4aa3b4f7606db805f0
Luke <novelview9@gmail.com>
2016-07-07 05:46:03 +0900

    updates usermodel : add usermanager

--- hoomi/users/models/user.py
+++ hoomi/users/models/user.py
@@ -1,10 +1,29 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 from django.db import models
-
 from jobs.models import Job
 
 
-class User(AbstractBaseUser):
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **kwargs):
+        user = self.model(
+                email=self.normalize_email(email),
+                **kwargs,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **kwargs):
+        user = self.create_user(
+                email=self.normalize_email(email),
+                **kwargs
+        )
+        user.is_admin = True
+        user.set_password(password)
+        user.save(using=self._db)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
     job = models.ForeignKey(Job)
     name = models.CharField(
             max_length=60,
@@ -13,6 +32,8 @@ class User(AbstractBaseUser):
             unique=True,
     )
     USERNAME_FIELD = 'email'
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
