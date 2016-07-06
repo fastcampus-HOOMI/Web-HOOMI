e863d22354c048f37a055a21e3169c426ddace4f
Luke <novelview9@gmail.com>
2016-07-06 20:04:38 +0900

    updates user models : make with base usermodel

--- hoomi/users/models/user.py
+++ hoomi/users/models/user.py
@@ -1,11 +1,18 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
 from jobs.models import Job
 
 
-class User(AbstractUser):
+class User(AbstractBaseUser):
     job = models.ForeignKey(Job)
     name = models.CharField(
             max_length=60,
     )
+    email = models.EmailField(
+            unique=True,
+    )
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
