baafbdb6b14cd596abaf10bceee0645bd3a00bdf
Luke <novelview9@gmail.com>
2016-07-06 17:35:42 +0900

    updates user app : add name linked with jobs, changed settings

--- hoomi/users/models/user.py
+++ hoomi/users/models/user.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from jobs.models import Job
 
 
 class User(AbstractUser):
-    pass
+    job = models.ForeignKey(Job)
+    name = models.CharField(
+            max_length=60,
+    )
