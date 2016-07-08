129cf0e9171670794ef42b4bd8c2bd7f12402162
LeoHeo <hjh5488@gmail.com>
2016-07-08 12:31:02 +0900

    init MobileLoginView
    기존에 rest_framework_jwt에서 기본 제공하는 obtain_jwt_token로 로그인을
    처리하다가 IOS Client에 좀 더 명시적으로 status_code를 뿌려주기 위해
    생성

--- hoomi/api/urls/mobile.py
+++ hoomi/api/urls/mobile.py
@@ -5,6 +5,6 @@ from rest_framework_jwt.views import obtain_jwt_token
 from api.views.mobile import *
 
 urlpatterns = [
-    url(r'^signup/$', UserCreateAPIView.as_view(), name="signup"),
-    url(r'^login/$', obtain_jwt_token, name="login"),
+    url(r'^signup/$', MobileSignupAPIView.as_view(), name="signup"),
+    url(r'^login/$', MobileLoginAPIView.as_view(), name="login"),
 ]
