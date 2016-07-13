39f8654147ed5046ac45038646f2082cbe615b48
LeoHeo <hjh5488@gmail.com>
2016-07-13 20:22:35 +0900

    change settings: authenticated, add default pagenation page size

--- hoomi/hoomi/settings/partials/api.py
+++ hoomi/hoomi/settings/partials/api.py
@@ -1,9 +1,10 @@
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
+    'PAGE_SIZE': 10,
 }
