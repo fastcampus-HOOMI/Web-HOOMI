ad071c9a132c7a1dc5b700726aa87e0421ed4511
LeoHeo <hjh5488@gmail.com>
2016-07-14 12:37:40 +0900

    fix usermanager

--- hoomi/users/models/user.py
+++ hoomi/users/models/user.py
@@ -1,7 +1,15 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
 from jobs.models import Job
+
+
+class HoomiUserManager(UserManager):
+    def get_or_none(self, **kwargs):
+        try:
+            return self.get(**kwargs)
+        except:
+            return None
 
 
 class User(AbstractUser):
