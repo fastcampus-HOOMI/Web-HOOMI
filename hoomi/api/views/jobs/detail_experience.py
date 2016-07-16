f02a9d968b00e19b2d9cba70fa7f6a064abc7d11
LeoHeo <hjh5488@gmail.com>
2016-07-16 13:05:00 +0900

    init job update: add patch job update
    job update 하는 API구성
    
    client에서 해당하는 job id를 patch로 보내주면 해당하는 job으로 업데이트

--- hoomi/api/views/jobs/detail_experience.py
+++ hoomi/api/views/jobs/detail_experience.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework import status
+
 from django.shortcuts import get_object_or_404
 
 from jobs.models import PhotoJobHistory
