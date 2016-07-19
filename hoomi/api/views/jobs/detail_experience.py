78f3075d124ad85ae95632ec4ea06817f1d748ed
LeoHeo <hjh5488@gmail.com>
2016-07-19 19:17:57 +0900

    add PhotoJob Patch, Destroy API
    수정 삭제는 본인이 작성한 글만 수정 삭제를 할 수 있음
    photo_job같은 경우에는 `Patch`, `Delete`에서 둘다 쓰기 때문에
    `get_photo_job` function 구현
    
    1. HTTP Patch로 업데이트 API 구현
    theme 말고는 다른 key값 같은경우에는 바뀌면 안되기 때문에
    theme 키 값만 뽑아온 후에 update
    
    Patch `/api/job-history/<hash_id>`로 실행
    
    Success
    - status code 200
    - model value response
    
    Fail
    - 해당 데이터가 없는 경우: status code 404
    
    2. HTTP Delete로 삭제 API 구현
    본인이 작성한글 삭제
    PhotoJob을 삭제하면 Cascading으로 Experience도 삭제됨
    
    Delete `/api/job-history/<hash_id>`로 실행
    
    Success
    - status code 204(성공하고 Return할 데이터가 없기 때문에)
    
    Fail
    - 해당 데이터가 없는 경우: status code 404

--- hoomi/api/views/jobs/detail_experience.py
+++ hoomi/api/views/jobs/detail_experience.py
@@ -1,21 +1,72 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import GenericAPIView, get_object_or_404
 from rest_framework.response import Response
+from rest_framework import mixins
 from rest_framework import status
-
-from django.shortcuts import get_object_or_404
 
 from jobs.models import PhotoJobHistory
 from api.paginations.detail import DetailPagination
 from api.serializers import ExperienceSerializer
 
 
-class ExperienceDetailListAPIView(ListAPIView):
+class ExperienceDetailListPatchDestroyAPIView(mixins.ListModelMixin,
+                                              GenericAPIView):
+
     serializer_class = ExperienceSerializer
     pagination_class = DetailPagination
     lookup_field = "hash_id"
 
     def get_queryset(self):
         hash_id = self.kwargs.get("hash_id")
-        photo = get_object_or_404(PhotoJobHistory, hash_id=hash_id)
+        photo_job = get_object_or_404(
+            PhotoJobHistory,
+            hash_id=hash_id,
+        )
 
-        return photo.experience_set.all()
+        return photo_job.experience_set.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def get_photo_job(self):
+        user = self.request.user
+        hash_id = self.kwargs.get("hash_id")
+        photo_job = get_object_or_404(
+            user.photojobhistory_set,
+            hash_id=hash_id,
+        )
+
+        return photo_job
+
+    def patch(self, request, *args, **kwargs):
+        photo_job = self.get_photo_job()
+
+        filter_key = {
+            key: value
+            for key, value
+            in request.data.items()
+            if key == "theme"
+        }
+
+        if filter_key:
+            photo_job.theme = filter_key.get("theme")
+            photo_job.save()
+
+        response_data = {
+            "username": photo_job.user.username,
+            "theme": photo_job.theme,
+        }
+
+        return Response(
+            response_data,
+            status.HTTP_200_OK,
+        )
+
+    def delete(self, request, *args, **kwargs):
+        photo_job = self.get_photo_job()
+
+        photo_job.delete()
+
+        return Response(
+            {},
+            status.HTTP_204_NO_CONTENT
+        )
