7a963ce28e777539ba2c44872960773c3b7e2a68
LeoHeo <hjh5488@gmail.com>
2016-07-25 20:09:06 +0900

    change Sentry DSN Key

--- hoomi/hoomi/settings/partials/raven.py
+++ hoomi/hoomi/settings/partials/raven.py
@@ -1,5 +1,6 @@
 import raven
+import os
 
 RAVEN_CONFIG = {
-    'dsn': 'https://780de6ff9af94160892a8fef16efeab0:55c0c3f187c645b2ba1147c7b4b578e6@app.getsentry.com/88217',
+    'dsn': os.environ.get("SENTRY_KEY")
 }
