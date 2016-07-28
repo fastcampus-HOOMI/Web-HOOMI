5854517b6a7b738c6fb7ff45b98c4fb07ab775c5
Luke <novelview9@gmail.com>
2016-07-28 18:19:13 +0900

    change jobs url with selection page

--- hoomi/jobs/urls.py
+++ hoomi/jobs/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 
 from jobs.views import *
 
-
 urlpatterns = [
     url(r'^$', MainView.as_view(), name="main"),
     url(r'^select/$', SelectView.as_view(), name="select"),
