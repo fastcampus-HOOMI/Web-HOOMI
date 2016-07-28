b9c5de3b3c9263b4fc2853bcf362c5a1a1570f33
Luke <novelview9@gmail.com>
2016-07-28 18:16:03 +0900

    change init file

--- hoomi/api/urls/__init__.py
+++ hoomi/api/urls/__init__.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url, include
 
-
 urlpatterns = [
     url(r'^', include('api.urls.auth', namespace="auth")),
     url(r'^token/', include('api.urls.token', namespace="tokens")),
